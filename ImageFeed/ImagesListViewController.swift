338496e0577fa8874946a52109456c0f589958f8
Vika Muravyova <muravyova.vi@ya.ru>
2024-08-04 19:44:14 +0300

    Изменены таблицы, добавлена реализация протоколов UITableViewDataSource и UITableViewDelegate

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,21 +1,27 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Vi Ka on 25.07.2024.
-//
-
 import UIKit
 
 class ImagesListViewController: UIViewController {
 
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
